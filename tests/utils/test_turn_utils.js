b61905d6df1931be363317fcf969dc94bf0ad2b4
Evan Snyder <evan@larky.com>
2020-02-13 12:46:49 -0500

    Adds emoji validation #3

--- tests/utils/test_turn_utils.js
+++ tests/utils/test_turn_utils.js
@@ -38,4 +38,44 @@ describe('Turn conversation utils', () => {
             should.not.exist(turn);
         });
     })
+    describe('isValidResponse', () => {
+        it('it should return false if type is text and message contains emoji characters', (done) => {
+            turnUtils.isValidResponse("Here's some good stuff, but then 👮👳🏄🧟👩‍🍳🧓🚑🚔🎒🅾️Hey it's me.", MessageType.text).should.equal(false);
+            done();
+        });
+        it('it should return true if type is text and message contains only alphanumeric characters', (done) => {
+            turnUtils.isValidResponse("Here's a normal sentence; it has cool punctuation!@**#(!).", MessageType.text).should.equal(true);
+            done();
+        });
+        it('it should return true if type is text and message contains only alphanumeric characters and spaces/newlines', (done) => {
+            turnUtils.isValidResponse(`Neat! Oh    <message_type>   </message_type> burrito
+
+                b;ajpowk`, MessageType.text).should.equal(true);
+            done();
+        });
+        it('it should allow all the weird characters', (done) => {
+            let weirdies = "•√π÷×¶∆£¢€¥^°={}\][✓%<>%/*-+0çxzlkjhgfdßàáâäæãåāèéēêë456ûúùüūîìïíīóøœōôöõòñ";
+            turnUtils.isValidResponse(weirdies, MessageType.text).should.equal(true);
+            done();
+        });
+        it('it should return false if type is emoji and message contains string characters', (done) => {
+            turnUtils.isValidResponse("Here's some good stuff, but then 👮👳🏄🧟👩‍🍳🧓🚑🚔🎒🅾️Hey it's me.", MessageType.emoji).should.equal(false);
+            done();
+        });
+        it('it should return true if type is emoji and message contains only emoji characters', (done) => {
+            turnUtils.isValidResponse("👮👳🏄🧟👩‍🍳🧓🚑🚔🎒", MessageType.emoji).should.equal(true);
+            done();
+        });
+        it('it should return true if type is emoji and message contains only emoji characters and spaces/newlines', (done) => {
+            turnUtils.isValidResponse(`👮👳🏄🧟  👩‍🍳🧓 
+
+                🚑🚔🎒`, MessageType.emoji).should.equal(true);
+            done();
+        });
+        it('it should block for a weird character', (done) => {
+            let weirdies = "👮👳🏄🧟  👩‍🍳🧓€";
+            turnUtils.isValidResponse(weirdies, MessageType.text).should.equal(false);
+            done();
+        });
+    })
 });
--- utils/turn_utils.js
+++ utils/turn_utils.js
@@ -1,10 +1,17 @@
 const MessageType = require('../types/message_type');
 const models = require('../models');
+const emojiRegex = require('emoji-regex');
+const emojiReg = emojiRegex();
+const textRegex = /[a-zA-Z0-9\.\!\+\$\#\@\_\&\-\+\(\)\/\*\"\'\:\;\!\?\~\`\|\•\√\π\÷\×\¶\∆\£\¢\€\¥\^\°\=\{\}\\\]\[\✓\%\<\>\%\/\*\-\+\ç\ß\à\á\â\ä\æ\ã\å\ā\è\é\ē\ê\ë\û\ú\ù\ü\ū\î\ì\ï\í\ī\ó\ø\œ\ō\ô\ö\õ\ò\ñ]+/
 
 module.exports = {
     isValidResponse: (response, messageType) => {
-        if (response != "BAD") {
-            return true;
+        response = response.replace(/\s+/g, '');
+        if (messageType == MessageType.text) {
+            return !emojiReg.test(response);
+        }
+        else if (messageType == MessageType.emoji) {
+            return !textRegex.test(response);
         }
         return false;
     },
