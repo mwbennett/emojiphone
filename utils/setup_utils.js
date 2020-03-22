2ee3203e0adc7e164083e26994e7c56cadd544c3
Evan Snyder <evan@larky.com>
2020-03-22 12:58:06 -0400

    Make game in database during setup #16

--- utils/setup_utils.js
+++ utils/setup_utils.js
@@ -3,6 +3,8 @@ const _ = require("underscore");
 const MessageType = require('../types/message_type');
 const models = require('../models');
 
+// TODO: async/awaitify this page
+
 module.exports = {
     MINIMUM_PLAYER_COUNT: 3,
     /**
@@ -11,6 +13,9 @@ module.exports = {
     * @param  {Object[]} existingUsers  List of users (each contained within it's own list of one item) already existing in the database to include in the game.
     */
     setupGame: (newUsers, existingUsers) => {
+        if (!existingUsers) {
+            existingUsers = [];
+        }
         let promises = [];
         for(let user of newUsers) {
             promises.push(models.user.upsert(user, {returning: true}).catch(err => {
@@ -27,26 +32,30 @@ module.exports = {
     * Create game turns given the players involved. Order should be random!
     * @param  {Object[]} dbUsers  List of users (each contained within it's own list of one item) from the database in the Sequelize format
     */
-    makeTurns: (dbUsers) => {
+    makeTurns: async (dbUsers) => {
         dbUsers = _.shuffle(dbUsers);
         let isCurrent = true;
         let messageType = MessageType.text;
-        return models.turn.max('gameId').then(maxGameId => {
-            if (!maxGameId) 
-                maxGameId = 0
-            maxGameId++;
-            let turnPromises = [];
-            for (var i = 0; i < dbUsers.length; i++) {
-                nextUserId = null;
-                if (i < dbUsers.length - 1) {
-                    nextUserId = dbUsers[i + 1][0].id;
-                }
-                turnPromises.push(module.exports.makeTurn(dbUsers[i][0], nextUserId, isCurrent, maxGameId, messageType));
-                isCurrent = false;
-                messageType = null;
+        let newGame;
+        try {
+            newGame = await models.game.create();
+        } catch(e) {
+            console.log(e);
+            return new Promise((resolve, reject) => {
+                reject("Could not create new game");
+            })
+        };
+        let turnPromises = [];
+        for (var i = 0; i < dbUsers.length; i++) {
+            nextUserId = null;
+            if (i < dbUsers.length - 1) {
+                nextUserId = dbUsers[i + 1][0].id;
             }
-            return Promise.all(turnPromises);
-        });
+            turnPromises.push(module.exports.makeTurn(dbUsers[i][0], nextUserId, isCurrent, newGame.id, messageType));
+            isCurrent = false;
+            messageType = null;
+        }
+        return Promise.all(turnPromises);
     },
     /**
     * Make a single turn in the game
